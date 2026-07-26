28a694316beca6547694c620bf1c2c0c0382bb48
Allan <allangomes_07@outlook.com>
2026-07-26 20:49:52 -0300

    calculadora básica concluída! HTML + CSS + JavaScript

---
+++ calculadora_numerica/js/calculadora_numerica.js
@@ -0,0 +1,21 @@
+function inserir(num){
+    let numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+function limpar(){
+    document.getElementById('resultado').innerHTML = " ";
+
+}
+function apagar(){
+    let resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+function calculo(){
+    let final = document.getElementById('resultado').innerHTML;
+    if (final){
+        document.getElementById('resultado').innerHTML = eval(final);
+    }
+    else{
+        document.getElementById('resultado').innerHTML = '...'
+    }
+}
